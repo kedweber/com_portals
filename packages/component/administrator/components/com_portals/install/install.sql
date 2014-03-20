16074fcf8138899306cdedb10319933c025fea16
jvdheijden <joep.van.der.heijden@moyoweb.nl>
2014-03-20 17:04:37 +0100

    Changed fieldset

--- packages/component/administrator/components/com_portals/install/install.sql
+++ packages/component/administrator/components/com_portals/install/install.sql
@@ -4,7 +4,7 @@
 CREATE TABLE IF NOT EXISTS `#__portals_portals` (
   `portals_portal_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `uuid` char(36) NOT NULL,
-  `cck_fieldset_id` int(11) NOT NULL DEFAULT '51',
+  `cck_fieldset_id` int(11) NOT NULL DEFAULT '1300',
   `title` varchar(255) NOT NULL,
   `slug` varchar(255) NOT NULL,
   `type` varchar(255) NOT NULL DEFAULT 'portal',
@@ -24,11 +24,11 @@ CREATE TABLE IF NOT EXISTS `#__portals_portals` (
 -- ----------------------------
 INSERT IGNORE INTO `#__cck_fieldsets` (`cck_fieldset_id`, `title`, `slug`, `enabled`, `created_on`, `created_by`, `modified_on`, `modified_by`, `locked_on`, `locked_by`)
 VALUES
-  (51,'Portal','portal',1,'0000-00-00 00:00:00',0,'0000-00-00 00:00:00',0,'2013-10-16 14:48:46',349);
+  (1300,'Portal','portal',1,'0000-00-00 00:00:00',0,'0000-00-00 00:00:00',0,'2013-10-16 14:48:46',349);
 
 -- ----------------------------
 --  Insert for `#__cck_fieldsets_elements`
 -- ----------------------------
 INSERT IGNORE INTO `#__cck_fieldsets_elements` (`cck_fieldset_id`, `cck_element_id`, `placholder`, `default`, `validator`, `required`, `ordering`, `enabled`, `created_on`, `created_by`, `modified_on`, `modified_by`, `locked_on`, `locked_by`)
 VALUES
-	(51,3,'','','',0,3,1,'2013-10-16 14:48:46',349,'0000-00-00 00:00:00',0,'0000-00-00 00:00:00',0);
+	(1300,6,'','','',0,3,1,'2013-10-16 14:48:46',349,'0000-00-00 00:00:00',0,'0000-00-00 00:00:00',0);
