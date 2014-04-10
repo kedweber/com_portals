461d9c88c0553efd04d62a08e1a7daa9b8cb69d9
Dave Li <dave.li@imagegroup.nl>
2014-04-10 16:41:24 +0200

    Improved install.sql.

--- packages/component/administrator/components/com_portals/install/install.sql
+++ packages/component/administrator/components/com_portals/install/install.sql
@@ -2,24 +2,25 @@
 --  Table structure for `#__portals_portals`
 -- ----------------------------
 CREATE TABLE IF NOT EXISTS `#__portals_portals` (
-  `portals_portal_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `portals_portal_id` int(11) NOT NULL AUTO_INCREMENT,
   `uuid` char(36) NOT NULL,
-  `portals_category_id` int(11) unsigned,
+  `portals_category_id` int(11) unsigned DEFAULT NULL,
   `cck_fieldset_id` int(11) NOT NULL DEFAULT '1300',
   `title` varchar(255) NOT NULL,
   `slug` varchar(255) NOT NULL,
-  `type` varchar(255) NOT NULL DEFAULT 'portal',
-  `ordering` bigint(20) unsigned NOT NULL,
+  `fields` text NOT NULL,
+  `ordering` int(11) NOT NULL,
   `enabled` tinyint(1) NOT NULL DEFAULT '1',
   `featured` tinyint(1) NOT NULL DEFAULT '0',
-  `created_on` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `created_by` int(11) NOT NULL DEFAULT '0',
-  `modified_on` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `modified_by` int(11) NOT NULL DEFAULT '0',
   `locked_on` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `locked_by` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`portals_portal_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `locked_by` bigint(20) NOT NULL DEFAULT '0',
+  `created_on` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created_by` bigint(20) NOT NULL DEFAULT '0',
+  `modified_on` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `modified_by` bigint(20) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`portals_portal_id`),
+  UNIQUE KEY `uuid` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 -- ----------------------------
 --  Table structure for `#__portals_categories`
