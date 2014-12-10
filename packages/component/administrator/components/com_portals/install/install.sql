d42fc12d14d515ef73135963354476355c05a192
jvdheijden <joep.van.der.heijden@moyoweb.nl>
2014-12-10 10:34:06 +0100

    Removed CCK references.

--- packages/component/administrator/components/com_portals/install/install.sql
+++ packages/component/administrator/components/com_portals/install/install.sql
@@ -39,17 +39,3 @@ CREATE TABLE IF NOT EXISTS `#__portals_categories` (
   `locked_by` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`portals_category_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
---  Insert for `#__cck_fieldsets`
--- ----------------------------
-INSERT IGNORE INTO `#__cck_fieldsets` (`cck_fieldset_id`, `title`, `slug`, `enabled`, `created_on`, `created_by`, `modified_on`, `modified_by`, `locked_on`, `locked_by`)
-VALUES
-  (1300,'Portal','portal',1,'0000-00-00 00:00:00',0,'0000-00-00 00:00:00',0,'2013-10-16 14:48:46',349);
-
--- ----------------------------
---  Insert for `#__cck_fieldsets_elements`
--- ----------------------------
-INSERT IGNORE INTO `#__cck_fieldsets_elements` (`cck_fieldset_id`, `cck_element_id`, `placeholder`, `default`, `validator`, `required`, `ordering`, `enabled`, `created_on`, `created_by`, `modified_on`, `modified_by`, `locked_on`, `locked_by`)
-VALUES
-	(1300,6,'','','',0,3,1,'2013-10-16 14:48:46',349,'0000-00-00 00:00:00',0,'0000-00-00 00:00:00',0);
